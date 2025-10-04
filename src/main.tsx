abd3212b012b41740edb17299e446fe7838db2d7
oligibbons <olipg@hotmail.co.uk>
2025-10-04 19:19:28 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter here
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { GameVersionProvider } from './contexts/GameVersionContext';
-import { ThemeProvider } from './hooks/useTheme';
+import { ThemeProvider } from './hooks/useTheme.tsx'; // Ensure path is to .tsx file
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
