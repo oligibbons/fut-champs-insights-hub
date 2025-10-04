681740b121e1ae42d7a110ce37385bdefb23d322
oligibbons <olipg@hotmail.co.uk>
2025-10-04 19:10:41 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter here
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { GameVersionProvider } from './contexts/GameVersionContext';
+import { ThemeProvider } from './hooks/useTheme';
+import './App.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* This is now the single router for the entire application */}
+    <BrowserRouter>
+      <AuthProvider>
+        <GameVersionProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </GameVersionProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
