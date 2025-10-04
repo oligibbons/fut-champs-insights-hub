27a83f9e3baca87867621a3ab153bed2255c25e9
oligibbons <olipg@hotmail.co.uk>
2025-10-04 13:43:26 +0100

    Update DashboardOverview.tsx

--- src/components/DashboardOverview.tsx
+++ src/components/DashboardOverview.tsx
@@ -4,7 +4,8 @@ import { GameResult } from "@/types/futChampions";
 import PerformanceRadar from "./PerformanceRadar";
 import MatchTagAnalysis from "./MatchTagAnalysis";
 import PositionalHeatMap from "./PositionalHeatMap";
-import { BarChart, LayoutGrid, Map } from "lucide-react";
+import GoalInvolvementChart from "./GoalInvolvementChart"; // Import the new component
+import { BarChart, LayoutGrid, Map, PieChart } from "lucide-react";
 
 interface DashboardOverviewProps {
   games: GameResult[];
@@ -18,10 +19,11 @@ const DashboardOverview = ({ games }: DashboardOverviewProps) => {
       </CardHeader>
       <CardContent>
         <Tabs defaultValue="radar" className="w-full">
-          <TabsList className="grid w-full grid-cols-3">
+          <TabsList className="grid w-full grid-cols-4"> {/* Updated to 4 columns */}
             <TabsTrigger value="radar"><LayoutGrid className="h-4 w-4 mr-2" />Radar</TabsTrigger>
             <TabsTrigger value="heatmap"><Map className="h-4 w-4 mr-2" />Heatmap</TabsTrigger>
             <TabsTrigger value="tags"><BarChart className="h-4 w-4 mr-2" />Match Tags</TabsTrigger>
+            <TabsTrigger value="goals"><PieChart className="h-4 w-4 mr-2" />Goals</TabsTrigger> {/* New Tab */}
           </TabsList>
           
           <TabsContent value="radar" className="mt-4">
@@ -32,6 +34,9 @@ const DashboardOverview = ({ games }: DashboardOverviewProps) => {
           </TabsContent>
           <TabsContent value="tags" className="mt-4">
             <MatchTagAnalysis games={games} />
+          </TabsContent>
+          <TabsContent value="goals" className="mt-4"> {/* New Tab Content */}
+            <GoalInvolvementChart />
           </TabsContent>
         </Tabs>
       </CardContent>
