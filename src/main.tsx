6960df767306762393b654152af2066e8aa3f57c
Oliver Perkins-Gibbons <oliverperkins-gibbons@Olivers-MacBook-Pro.local>
2025-10-31 17:26:32 +0000

    main.tsx update

--- src/main.tsx
+++ src/main.tsx
@@ -2,36 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
-import { GameVersionProvider } from './contexts/GameVersionContext';
-import { ThemeProvider } from './hooks/useTheme';
+import App from './App.tsx';
+import './index.css';
+
+// --- THIS WAS THE FIX ---
+// The import for ThemeProvider was using a colon (:) instead of "from".
+import { ThemeProvider } from './hooks/useTheme.tsx';
+// --- END FIX ---
+
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import { Toaster } from "@/components/ui/sonner";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-// --- FIX: Import DataSyncProvider here ---
-import { DataSyncProvider } from './hooks/useDataSync.tsx'; 
-import './index.css'; 
-
-// Create a client for react-query
-const queryClient = new QueryClient();
+import { DataSyncProvider } from './hooks/useDataSync.tsx';
+import { GameVersionProvider } from './contexts/GameVersionContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <GameVersionProvider>
-            <ThemeProvider>
-              {/* --- FIX: Wrap the entire App in DataSyncProvider --- */}
-              <DataSyncProvider>
-                <App />
-                <Toaster />
-              </DataSyncProvider>
-            </ThemeProvider>
-          </GameVersionProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <GameVersionProvider>
+          <AuthProvider>
+            <DataSyncProvider>
+              <App />
+              <Toaster />
+            </DataSyncProvider>
+          </AuthProvider>
+        </GameVersionProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
